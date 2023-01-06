d01918472d92d51d2c29740fa0157d0ebaadd93b
Ian-Mark <ianarapkirui@gmail.com>
2023-01-06 11:25:02 +0300

    create uri array to add

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -18,7 +18,7 @@ for (let i = 0; i < 101; i++) {
   years.push({ value: currentYear - i, label: currentYear - i });
 }
 const fillPlaylist = async (token, playlistId) => {};
-const searchForTracks = async (token, genre, year) => {
+const getTracks = async (token, genre, year) => {
   const searchResults = await fetcher(
     `https://api.spotify.com/v1/search?q=genre:${genre}%20year:${year}&type=track&limit=30`,
     {
@@ -29,6 +29,20 @@ const searchForTracks = async (token, genre, year) => {
   );
   console.log("search results:", searchResults);
   return searchResults;
+};
+
+const selectTracksToAdd = (trackInfoArray) => {
+  let totalDuration = 0;
+  let tracksToAdd = [];
+  for (let i = 0; i < trackInfoArray.length; i++) {
+    if (totalDuration + trackInfoArray[i].duration_seconds < 1800) {
+      tracksToAdd.push(trackInfoArray[i].uri);
+      totalDuration += trackInfoArray[i].duration_seconds;
+    } else {
+      break;
+    }
+  }
+  return tracksToAdd;
 };
 export default function Dropdown() {
   const { token } = useAuth();
@@ -58,7 +72,19 @@ export default function Dropdown() {
         }),
       });
       console.log(emptyPlaylist);
-      searchForTracks(token, selectedGenre, selectedYear);
+      const tracksResponse = await getTracks(
+        token,
+        selectedGenre,
+        selectedYear
+      );
+      const trackInfo = tracksResponse.tracks.items.map((track) => {
+        return {
+          uri: track.uri,
+          duration_seconds: Math.floor(track.duration_ms / 1000),
+        };
+      });
+      const tracksToAdd = selectTracksToAdd(trackInfo);
+      console.log("tracks to add:", tracksToAdd);
     } else {
       alert("Please select a genre and year");
     }
