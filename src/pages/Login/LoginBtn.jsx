121029960abc481492825634b497c584f04ff4b5
Ian-Mark <ianarapkirui@gmail.com>
2023-01-10 10:11:17 +0300

    redirect url: changed3

--- src/pages/Login/LoginBtn.jsx
+++ src/pages/Login/LoginBtn.jsx
@@ -38,7 +38,7 @@ const Login = () => {
       <div className={styles.container}>
         <SpotifyAuth
           noCookie={true}
-          redirectUri="https://spotify-playlist-maker-4e821.web.app"
+          redirectUri="https://spotify-playlist-maker-4e821.web.app/"
           clientID={spotifyClientKey}
           scopes={[
             Scopes.playlistModifyPrivate,
